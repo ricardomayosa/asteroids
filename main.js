38986979d5a4625d8f429c2888b6e142673528cb
Ricardo Mayo <rmayo.1995@gmail.com>
2018-08-30 20:56:37 -0500

    spaceship changes direction

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var playerImg = ['./images/player2.png', './images/player1.png', 'https://bit.ly/2wv84jf'];
+var degrees = 0;
 
-ctx.strokeRect(0,0,canvas.clientWidth, canvas.height);
+//ctx.strokeRect(0,0,canvas.width, canvas.height);
 
 var back = new Background();
+var player = new Player(300,250,50,120,playerImg[0]);
 back.selectImage();
-back.draw();
-// var frames = 0;
-// var interval = setInterval(function(){
-//     frames++;
-//     ctx.clearRect(0,0, 400, 200);
-//     back.draw();
-// }, 1000/60);
+
+var frames = 0;
+var interval = setInterval(function(){
+    frames++;
+    ctx.clearRect(0,0, 1000, 600);
+    back.draw();
+    player.rotate(degrees);
+}, 1000/60);
+
+addEventListener("keydown", function(event) {
+    switch(event.keyCode) {
+        // Up Arrow
+        case 38:
+            player.move();
+            break;
+        // Right Arrow    
+        case 39:
+            degrees += 15;
+            player.rotate(degrees);
+            break;
+        // Left Arrow
+        case 37:
+            degrees -= 15;
+            player.rotate(degrees);
+            break;
+        default:
+            break;
+    }
+});
+
--- player.js
+++ player.js
@@ -8,7 +8,29 @@ class Player extends Item {
         this.alive = false;
         console.log("Chocaste");
     }
+    move() {
+        this.y -= 20;
+        
+    }
     shoot() {
         console.log("PIU PIU");
+    }
+    rotate(degrees) {
+
+        ctx.save();
+        ctx.beginPath();
+        // move to the center of the canvas
+        ctx.translate(player.x + player.width/2, player.y + player.height/2);
+
+        // rotate the canvas to the specified degrees
+        ctx.rotate(degrees * Math.PI/180);
+
+        // draw the image
+        // since the context is rotated, the image will be rotated also
+        
+        ctx.drawImage(this.image, -this.width/2, -this.height/2, this.width, this.height);
+
+        // we’re done with the rotating so restore the unrotated context
+        ctx.restore();
     }
 }
